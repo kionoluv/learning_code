40b824164fc32db6236a6265b8305afebea4642c
Victoria Pham <kionoluv@users.noreply.github.com>
2016-05-10 01:36:22 -0400

    Add files via upload

---
+++ codecademy_lessons/Python/lists_and_directories.py
@@ -0,0 +1,23 @@
+"""
+=============================
+	LISTS AND DIRECTORIES
+=============================
+"""
+
+# LISTS #
+	#lists (a datatype) stores different pieces of info as sequence under a single variable name
+	#assign items to list:
+		list_name = [item_1, item_2] #items listed between hard brackets
+		empty_list = [] #lists can also be empty
+	
+	my_cravings = ["ice cream", "bubble tea", "berry-ful pancakes", "milkshake"];
+		if len(my_cravings) >= 3:
+			print "I really want some" + my_cravings[0]
+				#i really want some ice cream
+			print "I can't wait to have some" + my_cravings[1]
+				#i can't wait to have some bubble tea
+			print "It's been a long time since I made some" + my_cravings[2]
+				#it's been a long time since I made some berry-ful pancakes
+			print "Sometimes I just want a rich and delicious" + my_cravings[3]
+				#sometimes i just want a rich and delicious milkshake
+	
